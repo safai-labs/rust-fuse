d6a622b87beac3c77685a42b74a03e0f88bc9d2b
Ahmed Masud <ahmed.masud@saf.ai>
2019-09-19 01:07:47 -0400

    make rust-fuse locally compileable

--- fuse-sys/build.rs
+++ fuse-sys/build.rs
@@ -5,9 +5,13 @@ const LIBFUSE_NAME: &str = "fuse";
 const LIBFUSE_NAME: &str = "osxfuse";
 
 fn main() {
+    /*
     pkg_config::Config::new()
         .atleast_version("2.6.0")
         .probe(LIBFUSE_NAME)
         .map_err(|e| eprintln!("{}", e))
         .unwrap();
+    */
+    println!("cargo:rustc-link-search=native=-L../../../../libfuse/build/lib");
+    println!("cargo:rustc-link-lib=fuse");
 }
